95fe81163826f98af70f323c537a45f8ee9a7a22
muradmm83 <muradmm83@gmail.com>
2025-07-03 17:45:26 -0400

    Add: static code analysis

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
+"""
+This module contains the server
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask('Emotion Detection')
+app = Flask(__name__)
 
 @app.route('/emotionDetector')
 def analyze_emotion():
+    """
+    This function serves as a route for /emotionDetector
+    """
     text = request.args['textToAnalyze']
     response = emotion_detector(text)
 
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return 'Invalid text!'
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}. The dominant emotion is <b>{response['dominant_emotion']}</b>"
+    return f"""For the given statement, the system response is 'anger': {response['anger']},
+     'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']},
+     'sadness': {response['sadness']}. The dominant emotion is
+     <b>{response['dominant_emotion']}</b>"""
 
 @app.route('/')
 def index():
+    """
+    This function serves as a route for /
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
