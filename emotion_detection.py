eb4f60d0cb797f3ddab27c147e8f91d56e17136a
muradmm83 <muradmm83@gmail.com>
2025-07-03 16:09:03 -0400

    Add: format emotion result

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +6,18 @@ def emotion_detector(text_to_analyse):
     input = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=input, headers=headers)
+    formatted_response = json.loads(response.text)
+
+    data = formatted_response['emotionPredictions'][0]['emotion']
+    result = {
+        'anger': data['anger'],
+        'disgust': data['disgust'],
+        'fear': data['fear'],
+        'joy': data['joy'],
+        'sadness': data['sadness']
+    }
+
+    dominant_emotion = max(result, key=result.get)
+    result['dominant_emotion'] = dominant_emotion
     
-    return response.text
+    return result
